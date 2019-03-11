5e74c67d3bb7dce89ce631624d0b33398d262ca6
Matthew Butler <butler.matthew@gmail.com>
2019-03-11 10:54:39 -0300

    Start work on API

--- server/server.go
+++ server/server.go
@@ -4,17 +4,20 @@ import (
 	"net/http"
 )
 
-/// Server is the local HTTP server for configuring and viewing local stations.
+// Server is the local HTTP server for configuring and viewing local stations.
 type Server struct {
 	s *http.ServeMux
 }
 
+// New creates a new instance of Server
 func New() *Server {
 	s := &Server{s: http.NewServeMux()}
 	s.s.HandleFunc("/", s.index)
+	s.s.HandleFunc("/stations", s.stations)
 	return s
 }
 
+// ListenAndServe will start the Server listening on the specified port.
 func (s *Server) ListenAndServe(port string) error {
 	return http.ListenAndServe(port, s.s)
 }
@@ -27,4 +30,11 @@ func (s *Server) index(w http.ResponseWriter, req *http.Request) {
 
 	addr := req.URL.Path[1:]
 	http.ServeFile(w, req, "www/"+addr)
+}
+
+func (s *Server) stations(w http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/stations" {
+		http.NotFound(w, req)
+		return
+	}
 }
